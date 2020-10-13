ebbe92390d33aace9f861e05ad08b8d46ef01e69
Aryan Rathee <68062201+aryan-cell@users.noreply.github.com>
2020-10-13 09:08:51 +0530

    First PR

---
+++ 1-HelloProgram.cpp
@@ -0,0 +1,25 @@
+// Author:Aryan Rathee
+
+// Program to Print "Hello World" Program
+
+
+
+#include<iostream> 
+
+using namespace std; 
+
+// main function - 
+
+// where the execution of program begins 
+
+int main() 
+
+{ 
+
+	// prints hello world 	cout<<"Hello World"; 
+
+	
+
+	return 0; 
+
+} 
